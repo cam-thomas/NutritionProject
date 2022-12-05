fac3bb473773a9fbb689716cade4f219013ede64
Eric Zhang <zhange@umich.edu>
2022-12-05 16:25:30 -0500

    Added edit user info page.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,17 @@ import Image from "./doctor.png"
 
 /* NOTE: EDIT BUTTON AND LOGOUT BUTTONS ARE STILL BEING IMPLEMENTED */
 
+function FoodsEatenToday({foodEaten}) {
+  return (
+    <div className="foodsEatenToday">
+      <h3 className="foodsEatenHeader"> Foods Eaten Today </h3>
+    </div>
+  );
+}
+
+
+
+
 function RecommendedNutrients({userSignedIn, recommendedNutrition}) {
   /* TODO: Add units */
   if (userSignedIn) {
@@ -87,6 +98,76 @@ function FoodInputForm({userSignedIn, userinputFood, handleUserInputFood, handle
 }
 
 
+function EditForm({userInfo, setuserInfo, setIsEditButtonClicked}) {
+  const [genderInputValue, setgenderInputValue] = useState(userInfo.Gender)
+  const [heightInputValue, setHeightInputValue] = useState(userInfo.Height)
+  const [ageInputValue, setageInputValue] = useState(userInfo.Age)
+  const [weightInputValue, setweightInputValue] = useState(userInfo.Weight)
+
+  function handleUserEditGender(event){
+    setgenderInputValue(event.target.value)
+  }
+
+  function handleUserEditHeight(event) {
+    setHeightInputValue(event.target.value)
+  }
+
+  function handleUserEditAge(event) {
+    setageInputValue(event.target.value)
+  }
+
+  function handleUserEditWeight(event) {
+    setweightInputValue(event.target.value)
+  }
+
+  function submitEditForm(event) {
+    console.log("Submit clicked")
+    event.preventDefault()
+    setuserInfo({...userInfo, Gender: genderInputValue, Height: heightInputValue, Age: ageInputValue, Weight: weightInputValue})
+    setIsEditButtonClicked(false)
+  }
+
+  function handleCancelClick(event) {
+    setIsEditButtonClicked(false)
+  }
+
+  return (
+    <div className="editInfo"> 
+      <h3 className="editInfoHeader"> Edit Your Information </h3>
+      <div className="editOptions">
+        <form onSubmit={submitEditForm}> 
+          <label>
+            Gender:
+            <input className="editLabel" type="text" value={genderInputValue} onChange={handleUserEditGender}/>
+          </label>
+          <br/>
+          <label>
+            Age:
+            <input className="editLabel" type="text" value={ageInputValue} onChange={handleUserEditAge} />
+          </label>
+          <br/>
+          <label>
+            Height In Inches:
+            <input className="editLabel" type="text" value={heightInputValue} onChange={handleUserEditHeight}/>
+          </label>
+          <br/>
+          <label>
+            Weight:
+            <input className="editLabel" type="text" value={weightInputValue} onChange={handleUserEditWeight}/>
+          </label>
+        </form>  
+        <div className="cancelAndSubmitButtons">
+          <button onClick={handleCancelClick}> Cancel </button>
+          <button onClick={submitEditForm}>Submit</button>
+        </div>
+        
+      </div>
+    </div>
+  );
+}
+
+
+
 function App(props) {
   const [isEditButtonClicked, setIsEditButtonClicked] = useState(false)
   const [userSignedIn, setuserSignedIn] = useState(true)
@@ -95,7 +176,9 @@ function App(props) {
   const [foodEaten, setfoodEaten] = useState({})
   const [recommendedNutrition, setrecommendedNutrition] = useState({Calories: 0, Carbohydrates: 0, Fiber : 0, Protein: 0, Fat: 0, Water: 0})
   const [consumedNutrients, setconsumedNutrients] = useState({ Calories: 0, Carbohydrates: 0, Fiber: 0, Protein: 0, Fat: 0, Water: 0 })
-  
+  /* UserInfo dictionary may need to be updated based on backend */
+  const [userInfo, setuserInfo] = useState({Gender: "male", Height: "215", Weight: "150", Age: "22"})
+  const [userAccountInfo, setuserAccountInfo] = useState({Username: "", Password: "", Email: ""})
 
   function handleUserInputFood(event) {
     setuserinputFood(event.target.value)
@@ -139,9 +222,9 @@ function App(props) {
         </div>) : null}
       </div>
       {isEditButtonClicked ? (
-        <div className="editInfo"> </div>
+        <EditForm userInfo={userInfo} setuserInfo={setuserInfo} setIsEditButtonClicked={setIsEditButtonClicked} />
       ) : null}
-      <div className="bodyOne">
+      <div className="bodyOne" style={{display : isEditButtonClicked ? 'none' : 'block'}}>
         <div className="improveHeadline">
           <h1 id="improveNutrition">Improve your Nutrition</h1>
           <h2 id="improveDes">Using the foods you've eaten, we calculate what nutrients you lack or have in excess</h2>
@@ -158,6 +241,7 @@ function App(props) {
         </div>
         <RecommendedNutrients userSignedIn={userSignedIn} recommendedNutrition={recommendedNutrition}/>
         <NutrientsEaten userSignedIn={userSignedIn} nutrientsEaten={consumedNutrients} recommendedNutrition={recommendedNutrition} />
+        {/* <FoodsEatenToday foodEaten={foodEaten} /> */}
       </div>
     </div>
   );
