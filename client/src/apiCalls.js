4e848d4e40cfe7ba5f7bce1671eed0205fc987ad
Pedro Madriz <pmadriz-zepeda@intrepidcs.com>
2022-12-07 00:07:02 -0500

    update apiCalls.js

--- client/src/apiCalls.js
+++ client/src/apiCalls.js
@@ -20,30 +20,56 @@ export const registerUserCall = async (userInfo) => {
   console.log('Response from create account: ', JSON.stringify(data))
   return data
 }
-// response = {
+// example response = {
 //   dailyNutrients: {
-//     calories: 2197,
-//     carbs: 247,
-//     protein: 64,
-//     fat: 49,
+//     Calories: 2197,
+//     Carbohydrates: 247,
+//     Protein: 64,
+//     Fat: 49,
+//     Fiber: 25
 //   },
 //   currentNutrients: {
-//     calories: 0,
-//     carbs: 0,
-//     protein: 0,
-//     fat: 0,
+//     Calories: 0,
+//     Carbohydrates: 0,
+//     Protein: 0,
+//     Fat: 0,
+//     Fiber: 0
 //   },
 //   recommendedFoods: [
 //     {
-//       foodName: "Chicken & Rice",
-//       calories: 532,
-//       carbs: 22,
-//       protein: 68,
-//       fat: 17,
+//       foodName: 'Bacon',
+//       Calories: 161,
+//       Carbohydrates: 1,
+//       Protein: 12,
+//       Fat: 12,
+//       Fiber: 0
 //     },
-//      NOTE: 3 more of these food objects are sent back.. leaving them out to keep this short
-//   ],
-// };
+//     {
+//       foodName: 'Steak',
+//       Calories: 610,
+//       Carbohydrates: 0,
+//       Protein: 58,
+//       Fat: 41,
+//       Fiber: 0
+//     },
+//     {
+//       foodName: 'Apple',
+//       Calories: 95,
+//       Carbohydrates: 25,
+//       Protein: 1,
+//       Fat: 1,
+//       Fiber: 4
+//     },
+//     {
+//       foodName: 'Chicken Caesar Salad',
+//       Calories: 508,
+//       Carbohydrates: 14,
+//       Protein: 48,
+//       Fat: 28,
+//       Fiber: 3
+//     }
+//   ]
+// }
 
 //****** LOGIN USER (KAI) ********/
 // loginCreds = {
@@ -61,6 +87,57 @@ export const loginUserCall = async (loginCreds) => {
   return data
 }
 
+// example response = {
+//   dailyNutrients: {
+//     Calories: 2873,
+//     Carbohydrates: 323,
+//     Protein: 64,
+//     Fat: 64,
+//     Fiber: 30
+//   },
+//   currentNutrients: {
+//     Calories: 1573,
+//     Carbohydrates: 150,
+//     Protein: 20,
+//     Fat: 21,
+//     Fiber: 17
+//   },
+//   recommendedFoods: [
+//     {
+//       foodName: 'Banana',
+//       Calories: 105,
+//       Carbohydrates: 27,
+//       Protein: 1,
+//       Fat: 1,
+//       Fiber: 3
+//     },
+//     {
+//       foodName: 'Fried Eggs',
+//       Calories: 180,
+//       Carbohydrates: 1,
+//       Protein: 13,
+//       Fat: 13,
+//       Fiber: 0
+//     },
+//     {
+//       foodName: 'Turkey Sandwich',
+//       Calories: 324,
+//       Carbohydrates: 29,
+//       Protein: 21,
+//       Fat: 13,
+//       Fiber: 2
+//     },
+//     {
+//       foodName: 'Bacon',
+//       Calories: 161,
+//       Carbohydrates: 1,
+//       Protein: 12,
+//       Fat: 12,
+//       Fiber: 0
+//     }
+//   ]
+// }
+
 //****** LOGOUT USER ********/
 export const logoutCall = async () => {
   const res = await fetch('http://localhost:8080/api/v1/auth/logout', {
@@ -69,9 +146,11 @@ export const logoutCall = async () => {
   })
   // alert response code or something if needed..
 }
+// no response needed for frontend
 
-//****** ADD FOOD  ********/
-// addedFood = "Chicken & Rice" // just needs to be the name (string) of one of the foods in our backend foodList
+//****** ADD FOOD (Kai)  ********/
+// example for Kai
+// addedFood = "Turkey Sanwich" // just needs to be the name (string) of one of the foods in our backend foodList
 export const addFoodCall = async (addedFood) => {
   const res = await fetch('http://localhost:8080/api/v1/users/addFood', {
     method: 'put',
@@ -85,3 +164,52 @@ export const addFoodCall = async (addedFood) => {
   console.log('Response from adding food: ', JSON.stringify(data))
   return data
 }
+
+// example response = {
+//   nutrients: {
+//     Calories: 1897,
+//     Carbohydrates: 179,
+//     Protein: 41,
+//     Fat: 34,
+//     Fiber: 19
+//   },
+//   recommendedFoods: [
+//     {
+//       foodName: 'Banana',
+//       Calories: 105,
+//       Carbohydrates: 27,
+//       Protein: 1,
+//       Fat: 1,
+//       Fiber: 3
+//     },
+//     {
+//       foodName: 'Steak',
+//       Calories: 610,
+//       Carbohydrates: 0,
+//       Protein: 58,
+//       Fat: 41,
+//       Fiber: 0
+//     },
+//     {
+//       foodName: 'Turkey Sandwich',
+//       Calories: 324,
+//       Carbohydrates: 29,
+//       Protein: 21,
+//       Fat: 13,
+//       Fiber: 2
+//     },
+//     {
+//       foodName: 'Fruit Salad',
+//       Calories: 97,
+//       Carbohydrates: 24,
+//       Protein: 1,
+//       Fat: 1,
+//       Fiber: 3
+//     }
+//   ]
+// }
+
+// Note: currently backend isn't keeping track of "FoodsEaten" we should probably
+// be keeping track of this and return it in the log in response..
+// for now lets worry about getting everything to update correctly for our scenarios tho..
+// its a simple fix tho
